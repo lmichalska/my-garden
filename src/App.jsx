fe8501d0af3733ac5b2dc8f305f64acb246fa342
Lidia Michalska <144784986+lmichalska@users.noreply.github.com>
2024-11-08 17:27:11 +0100

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,21 @@ import Learn from "./Pages/Learn"
 import Acc from './Pages/Acc';
 import Contact from './Pages/Contact';
 import Community from './Pages/Community';
+import ScrollToTop from './Components/scroll';
+import Plantabase from './Pages/Plantabase';
+
+
+
 function App() {
   return (
     <main>
       <NavBar />
+      <ScrollToTop />
       <Routes>
       <Route path="/" element={<HomePage/>}/>
      <Route path="/Learn" element={<Learn/>}/>
      <Route path="/acc" element={<Acc/>}/>
+     <Route path="/plantabase" element={<Plantabase />}/>
      <Route path="/contact" element={<Contact/>}/>
      <Route path="/community" element={<Community/>}/>
      </Routes>
---
+++ src/Pages/Plantabase.jsx
@@ -0,0 +1,19 @@
+//Lidia
+
+import React from "react";
+import "..//Pages/Pages.css";
+
+function Plantabase() {
+  return (
+    <div className="app">
+      <main className="content">
+        <h1>Learn more about gardening!</h1>
+        
+        <div className="articles">
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Plantabase;
